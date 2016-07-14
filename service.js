354362ca2643f534adb19aaee83cd561b615d55f
Janis Krasemann <janis.krasemann@enpit.de>
2016-07-14 17:24:29 +0200

    Implement the service

---
+++ service.js
@@ -0,0 +1,22 @@
+const restify = require('restify');
+const fs = require('fs');
+
+const app = restify.createServer();
+const PORT = process.env.PORT || 8082;
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  next();
+});
+
+app.get('/:filename', (req, res) => {
+  debugger;
+  fs.readFile('./data/' + req.params.filename, 'utf-8', (err, data) => {
+    if (!err) res.json(data);
+  });
+});
+
+app.listen(PORT, function() {
+  console.log('service listening at port %s', PORT);
+});
